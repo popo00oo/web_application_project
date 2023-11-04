20155d8423bb423e5ec166d02cb986f330117fbc
popo00oo <jasonshen6@gmail.com>
2023-11-03 23:25:03 -0400

    login status status optimization

--- lost/forms.py
+++ lost/forms.py
@@ -2,16 +2,3 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-
-# Form for user registration
-class RegisterForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password1', 'password2']
-
-
-# Form for user login
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
-
